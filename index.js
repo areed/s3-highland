4f44aae6e62c0709e537ef4c0bcd92989d883588
Andrew Reed <w.andrew.reed@gmail.com>
2014-12-11 14:40:35 +0700

    several basic calls wrapped in streams

--- index.js
+++ index.js
@@ -1,144 +1,204 @@
+/** 
+ * A highland streams wrapper around the JavaScript AWS S3 API.
+ * @module S3
+ */
 var _ = require('highland');
+var File = require('vinyl');
+var extend = require('highlandx/extend');
 var pick = require('highlandx/pick');
 
-/**
- * Creates a new virtual s3Object.
- * @class
- * @param{Object} s3service - the service object to use to read or write this file
- * on S3. It should be an instance of AWS.S3
- * @param{Object} attrs - the virtual s3 object attributes
- */
-function S3Object(s3service, attrs) {
-  return _.extend(attrs, {
-    s3: s3service,
-    Bucket: '',
-    Key: '',
-    Body: null
-  });
-}
+/** @typedef {Object} HighlandStream */
 
-/**
- * Curryable wrapper of the constructor.
- */
-var newS3Object = exports.newS3Object = function(s3service, attrs) {
-  return new S3Object(s3service, attrs);
-};
-
-/**
- * Higher-order function to wrap s3 methods that don't require any
- * modifications.
- * @param{String} methodKey - name of the method to wrap
- */
-function wrap(methodKey) {
-  return function(s3service, params) {
-    return _.wrapCallback(s3service[methodKey].bind(s3service))(params);
-  };
-}
-
-//all the methods that can be wrapped simply with the wrap function
-[
-  'createBucket',
-  'deleteBucket'
-].forEach(function(methodKey) {
-  exports[methodKey] = wrap(methodKey);
-});
-
-/**
- * Higher-order function to wrap s3 methods that operate on objects from a
- * stream.
- * @param {string} methodKey - name of the method to wrap
- * @param {string[]} paramKeys - allowed params for the method
- * @return {object} a stream
- */
-function adapt(methodKey, paramKeys) {
-  var sanitizeParams = _.curry(pick, paramKeys);
-
-  return function(s3Obj) {
-    var s3Service = s3Obj.s3;
-    var call = _.wrapCallback(s3Service[methodKey].bind(s3Service));
-
-    return sanitizeParams(s3Obj)
-    .map(function(p) {
-      return p;
-    })
-    .map(call)
-    .series()
-    .map(function(data) {
-      return _.extend(data, s3Obj);
-    });
-  };
-}
-
-exports.deleteObject = adapt('deleteObject', [
+var pickDeleteObjectParams = _.curry(pick, [
   'Bucket',
   'Key',
+  'MFA',
+  'VersionId',
 ]);
 
-exports.putObject = function(s3Obj) {
-  var params = _.curry(pick, [
-    'ACL',
-    'Body',
-    'Bucket',
-    'CacheControl',
-    'ContentDisposition',
-    'ContentEncoding',
-    'ContentLanguage',
-    'ContentLength',
-    'ContentMD5',
-    'ContentType',
-    'Expires',
-    'GrantFullControl',
-    'GrantRead',
-    'GrantReadACP',
-    'GrantWriteACP',
-    'Key',
-    'Metadata',
-    'ServerSideEncryption',
-    'StorageClass',
-    'WebsiteRedirectLocation',
-    'SSECustomerAlgorithm',
-    'SSECustomerKey',
-    'SSECustomerKeyMD5'
-  ]);
-
-  return _(function(push, next) {
-    params(s3Obj).each(function(params) {
-      s3Obj.s3.putObject(params, function(err, data) {
-        if (err) {
-          push(err, data);
-        } else {
-          push(null, s3Obj);
-        }
-        push(null, _.nil);
-      });
-    });
-  });
-};
+var pickPutObjectParams = _.curry(pick, [
+  'Bucket',
+  'Key',
+  'ACL',
+  'Body',
+  'CacheControl',
+  'ContentDisposition',
+  'ContentEncoding',
+  'ContentLanguage',
+  'ContentLength',
+  'ContentMD5',
+  'ContentType',
+  'Expires',
+  'GrantFullControl',
+  'GrantRead',
+  'GrantReadACP',
+  'GrantWriteACP',
+  'Metadata',
+  'SSECustomerAlgorithm',
+  'SSECustomerKey',
+  'SSECustomerKeyMD5',
+  'SSEKMSKeyId',
+  'ServerSideEncryption',
+  'StorageClass',
+  'WebsiteRedirectLocation',
+]);
 
 /**
- * Lists all the keys in a bucket and handles paging transparently.
- * @param{Object} s3service - the service object to use to read or write this file
- * on S3. It should be an instance of AWS.S3
- * @param{Object} params - Bucket is required
- * be located
- * @return{Object} stream of s3Objects with empty body attributes
+ * @constructor
+ * @alias module:S3
+ * @param {Object} service - an instance of require('aws-sdk').S3
  */
-exports.listObjects = function(s3service, params) {
-  return _(function(push, next) {
-    var s3ObjFactory = _.curry(newS3Object, s3service);
+function S3(service) {
 
-    s3service.listObjects(params).eachItem(function(err, data) {
-      if (err) {
-        push(err);
-        push(null, _.nil);
-        return;
-      }
-      //data will be null when all items have been listed
-      if (data === null) {
-        push(null, _.nil);
-        return;
-      }
-      push(null, s3ObjFactory(_.extend({Bucket: params.Bucket}, data)));
-    });
-  });
+  /**
+   * A stream wrapper around the createBucket call.
+   * @function
+   * @param {Object} params -
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#createBucket-property
+   * @return {HighlandStream}
+   */
+  this.createBucket = _.wrapCallback(service.createBucket.bind(service));
+
+  /**
+   * A stream wrapper around the deleteBucket call.
+   * @function
+   * @param {Object} params -
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteBucket-property
+   * @return {HighlandStream}
+   */
+  this.deleteBucket = _.wrapCallback(service.deleteBucket.bind(service));
+
+  var put = _.wrapCallback(service.putObject.bind(service));
+
+  /**
+   * Uploads an object to an S3 bucket. It whitelists the legal params for the
+   * call so you can pass in streams from a variety of sources.
+   * @function
+   * @memberof S3
+   * @param {Object} params -
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+   * @return {HighlandStream} although the S3 call returns an object containing only an
+   * ETag, the object in this stream will also have all passed-in parameters for
+   * convenient piping.
+   */
+  var putObject = this.putObject = function(params) {
+    return put(pickPutObjectParams(params))
+      .map(_.extend(params));
+  };
+
+  var del = _.wrapCallback(service.deleteObject.bind(service));
+
+  /**
+   * Deletes an object from an S3 bucket. The bucket must be empty. Whitelists
+   * legal params for the call so you can pass in streams from a variety of
+   * sources.
+   * @param {Object} params -
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteObject-property
+   * @return {HighlandStream} will consist of one item: the original params 
+   * extended with any properties returned from S3 (DeleteMarker and VersionId).
+   */
+  this.deleteObject = function(params) {
+    return del(pickDeleteObjectParams(params))
+      .map(_.extend(params));
+  };
+
+  /**
+   * Calculates and whitelists allowed parameters from a Vinyl virtual file
+   * object and then uploads it to an S3 bucket.
+   * @param {Object} file - Must have a Bucket property. Body will be taken from
+   * the contents property and Key will be calculated from base and path
+   * properties if needed.
+   * @return {HighlandStream} contains one item: the original file extended with
+   * Body and Etag properties.
+   */
+  this.putVinylObject = function(file) {
+    var o = _.extend({
+      Key: key(file),
+      Body: file.contents,
+    }, pickPutObjectParams(file));
+
+    return putObject(o)
+      .map(function(etagged) {
+        return _.extend(etagged, file);
+      });
+  };
+
+  /**
+   * Like listObjects but returns a stream where each item is an object in the
+   * bucket.
+   * @param {Object} params - can contain any legal parameters for listObjects.
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listObjects-property
+   * @return {HighlandStream}
+   */
+  this.streamBucketContents = function(params) {
+    var objs = _();
+
+    service
+      .listObjects(params)
+      .eachItem(function(err, data) {
+        if (err) {
+          objs.write(err);
+          return;
+        }
+        if (data === null) {
+          objs.end();
+          return;
+        }
+        objs.write(_.extend({Bucket: params.Bucket}, data));
+      });
+ 
+    return objs;
+  };
+
+  /**
+   * A stream wrapper around the listBuckets call.
+   * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listBuckets-property
+   * @return {HighlandStream} a stream of the data.Buckets array (data.Owner is
+   * discarded). Each item will have Name and CreationDate properties, so map
+   * the Name property to Bucket property with nameToBucket before passing the
+   * stream to deleteBucket or streamBucketContents.
+   */
+  this.streamS3Buckets = function() {
+    var buckets = _();
+
+    service
+      .listBuckets()
+      .eachItem(function(err, data) {
+        if (err) {
+          buckets.write(err);
+          return;
+        }
+        if (data === null) {
+          buckets.end();
+          return;
+        }
+        buckets.write(data);
+      });
+
+    return buckets;
+  };
+
+}
+
+/**
+ * {Name: 'my-bucket', CreationDate: XXXX} => {Bucket: 'my-bucket'};
+ * Useful for mapping over the stream returned from streamS3Buckets before
+ * passing to calls that expect a Bucket param.
+ * @param {Object} bucket - should have a "Name" property
+ * @return {Object} will have a single property "Bucket"
+ */
+S3.prototype.nameToBucket = function(bucket) {
+  return {Bucket: bucket.Name};
 };
+
+module.exports = S3;
+
+function key(file) {
+  if (file.Key) {
+    return file.Key;
+  }
+  if (file.path.indexOf(file.base) !== 0 || file.path.length === file.base.length) {
+    throw new Error('Cannot calculate Key from base and path.');
+  }
+  return file.path.substring(file.base.length);
+}
--- test/index.js
+++ test/index.js
@@ -1,108 +1,206 @@
-var AWS = require('aws-sdk');
 var expect = require('chai').expect;
 var _ = require('highland');
+var AWS = require('aws-sdk');
+AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'areedio'});
+var service = new AWS.S3({apiVersion: '2006-03-01'});
+var S3 = require('../');
+var s3 = new S3(service);
+var File = require('vinyl');
+var prefix = 's3-highland-test';
+var index = 0;
 
-var _s3 = require('../index');
+describe('s3-highland', function() {
+  this.timeout(120000);
 
-var AccessKeyID = process.env.BUCKET_STREAM_ACCESS_KEY_ID;
-var SecretAccessKey = process.env.BUCKET_STREAM_SECRET_ACCESS_KEY;
-var creds = {
-  accessKeyId: AccessKeyID,
-  secretAccessKey: SecretAccessKey
-};
-var s3 = new AWS.S3(_.extend({apiVersion: '2006-03-01'}, creds));
+  after(function(done) {
+    var empty = function(bucket) {
+      return _(function(push, next) {
+        s3.streamBucketContents({Bucket: bucket})
+          .map(s3.deleteObject)
+          .series()
+          .last()
+          .apply(function() {
+            push(null, {Bucket: bucket});
+            push(null, _.nil);
+          });
+      });
+    };
 
-describe('listObjects', function() {
-  var count = 30;
-  var parallelism = 3;
-  var bucket;
+    s3
+      .streamS3Buckets()
+      .pluck('Name')
+      .filter(function(bucket) {
+        return bucket.substring(0, prefix.length) === prefix;
+      })
+      .flatMap(empty)
+      .flatMap(s3.deleteBucket)
+      .errors(function(err, rethrow) {
+        console.log(err);
+      })
+      .apply(function() {
+        done();
+      });
+  });
 
-  this.timeout(count * 2500);
+  describe('createBucket', function() {
+    var bucket = bucketName();
 
-  before(function(done) {
-    var populateX = _.curry(populate, count);
-
-    getTestBucket()
-    .flatMap(populateX)
-    .map(_s3.putObject)
-    .parallel(parallelism)
-    .toArray(function(objs) {
-      bucket = objs[0].Bucket;
-      done();
+    it('should end after the bucket is created.', function(done) {
+      s3.createBucket({
+        Bucket: bucket
+      })
+      .apply(function(data) {
+        expect(data).to.have.property('Location', '/' + bucket);
+        done();
+      });
     });
   });
 
-  after(function(done) {
-    if (bucket) {
-      disposeBucket(bucket)
-      .toArray(function() {
-        done();
+  describe('deleteBucket', function() {
+    var bucket = prepareBucket();
+
+    describe('given the bucket is empty', function() {
+      it('should delete the bucket.', function(done) {
+        s3
+          .deleteBucket({
+            Bucket: bucket
+          })
+          .errors(done)
+          .apply(function(data) {
+            expect(data).to.deep.equal({});
+            done();
+          });
       });
-      return;
-    }
-    done(null);
+    });
   });
 
-  it('should list all keys in the bucket', function(done) {
-    var index = 0;
-    var seen = {};
+  describe('putObject', function() {
+    var bucket = prepareBucket();
 
-    _s3.listObjects(s3, {Bucket: bucket}).each(function(s3Obj) {
-      expect(s3Obj).to.have.property('Key');
-      expect(s3Obj.Key).to.have.length.above(7);
-      expect(seen[s3Obj.Key]).to.equal(undefined);
-      seen[s3Obj.Key] = true;
-      expect(s3Obj).to.have.property('Body', null);
-      index++;
-      if (index === count) done();
+    it('should upload the object to S3.', function(done) {
+      var file = { Bucket: bucket, Key: 'a', Body: 'A' };
+
+      s3.putObject(file)
+        .errors(done)
+        .apply(function(o) {
+          expect(o).to.have.property('Bucket', bucket);
+          expect(o).to.have.property('Key', 'a');
+          expect(o).to.have.property('Body', 'A');
+          expect(o).to.have.property('ETag');
+          done();
+        });
+    });
+  });
+
+  describe('putVinylObject', function() {
+    var bucket = prepareBucket();
+
+    it('should upload the object to S3.', function(done) {
+      var file = new File({
+        base: '/public/',
+        path: '/public/index.html',
+        contents: Buffer('My Home Page.'),
+      });
+
+      file.Bucket = bucket;
+
+      s3.putVinylObject(file)
+        .errors(done)
+        .apply(function(f) {
+          expect(f).to.have.property('Bucket', bucket);
+          expect(f).to.have.property('Key', 'index.html');
+          expect(f).to.have.property('Body');
+          expect(f.Body.toString()).to.equal('My Home Page.');
+          expect(f).to.have.property('ETag');
+          expect(f).to.have.property('base', '/public/');
+          expect(f).to.have.property('path', '/public/index.html');
+          expect(f).to.have.property('contents');
+          expect(f).to.be.an.instanceof(File);
+          done();
+        });
+    });
+  });
+
+  describe('streamBucketContents', function() {
+    var bucket = prepareBucket();
+    var objects = populateBucket(bucket);
+
+    it('should return a stream containing metadata for each object in the bucket.', function(done) {
+      s3.streamBucketContents({Bucket: bucket})
+        .toArray(function(contents) {
+          expect(contents[0]).to.have.property('Key', objects[0].Key);
+          expect(contents[1]).to.have.property('Key', objects[1].Key);
+          done();
+        });
+    });
+  });
+
+  describe('deleteObject', function() {
+    var bucket = prepareBucket();
+    var obj = populateBucket(bucket, 1);
+
+    it('should delete the object from the bucket on S3.', function(done) {
+      s3.deleteObject({Bucket: bucket, Key: obj[0].Key})
+        .toArray(function() {
+          done();
+        });
+    });
+  });
+
+  describe('streamS3Buckets', function() {
+    var bucket = prepareBucket();
+
+    it('should list all your buckets.', function(done) {
+      var prefix = 's3-highland-test';
+
+      s3.streamS3Buckets()
+        .pluck('Name')
+        .find(function(b) {
+          return b === bucket;
+        })
+        .toArray(function(buckets) {
+          expect(buckets).to.have.length(1);
+          done();
+        });
     });
   });
 });
 
-function getTestBucket() {
-  var bucket = 's3-highland-test-' + new Date().valueOf();
-  return _s3.createBucket(s3, {Bucket: bucket}).map(function(bucket) {
-    return bucket.Location.slice(1);
+function bucketName() {
+  index++;
+  return 's3-highland-test-' + index + '-' + Date.now();
+}
+
+function prepareBucket() {
+  var bucket = bucketName();
+
+  before(function(done) {
+    s3
+      .createBucket({Bucket: bucket})
+      .errors(done)
+      .apply(function() {
+        done();
+      });
   });
+
+  return bucket;
 }
 
-function disposeBucket(bucket) {
-  return emptyBucket(bucket)
-  .reduce1(noop)
-  .flatMap(function() {
-    return _s3.deleteBucket(s3, {Bucket: bucket});
+function populateBucket(bucket, count) {
+  var files = [
+    { Bucket: bucket, Key: 'a', Body: 'apple' },
+    { Bucket: bucket, Key: 'b', Body: 'banana' },
+  ];
+
+  before(function(done) {
+    _(files.slice(0, count || files.length))
+      .map(s3.putObject)
+      .series()
+      .errors(done)
+      .toArray(function() {
+        done();
+      });
   });
-}
 
-function emptyBucket(bucket) {
-  return _s3.listObjects(s3, {Bucket: bucket})
-  .map(_s3.deleteObject)
-  .parallel(3);
+  return files;
 }
-
-function loremKey(index) {
-  return 'itemKey' + index;
-}
-
-function loremBody(index) {
-  return 'body' + index;
-}
-
-function dummy(bucket, index) {
-  return _s3.newS3Object(s3, {
-    Bucket: bucket,
-    Key: loremKey(index),
-    Body: loremBody(index)
-  });
-}
-
-function populate(count, bucket) {
-  return _(function(push, next) {
-    for (var i = 0; i < count; i++) {
-      push(null, dummy(bucket, i));
-    }
-    push(null, _.nil);
-  });
-}
-
-function noop() {}
